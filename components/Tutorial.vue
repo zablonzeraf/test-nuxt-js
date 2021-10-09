6a5e233aa055b08a88ad26212658874a3f2e6822
abenikeb <59362713+abenikeb@users.noreply.github.com>
2021-10-09 12:45:37 +0300

    Update Tutorial.vue

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -10,6 +10,9 @@
         <h2 class="text-2xl leading-7 font-semibold">
           Welcome to your Nuxt Application
         </h2>
+        <h2 class="text-2xl leading-7 font-semibold">
+          Welcome to your Nuxt Application
+        </h2>
         <p class="mt-3 text-gray-600">
           We recommend you take a look at the <a href="https://nuxtjs.org" target="_blank" class="text-green-500 hover:underline">Nuxt documentation</a>, whether you are new or have previous experience with the framework.<br>
         </p>
